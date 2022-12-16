464476797a0798a215ed51864dbc619924c558de
Luo Wei <luowei@dynamic-oit-vapornet100-10-8-57-188.princeton.edu>
2022-12-15 22:08:08 -0500

    add aggregation and execution w/ models

---
+++ federated/aggrandexec/commons.py
@@ -0,0 +1,15 @@
+
+DUMMY_RESPONSE = 'N'
+# Define Basic Experiment Setup and FL Events
+SIMULATION_MODE = 'simulation'
+DEPLOYMENT_MODE = 'deployment'
+TENSORFLOW = 'tensorflow'
+PYTORCH = 'pytorch'
+UPDATE_MODEL = 'update_model'
+MODEL_TEST = 'model_test'
+SHUT_DOWN = 'terminate_executor'
+START_ROUND = 'start_round'
+CLIENT_CONNECT = 'client_connect'
+CLIENT_TRAIN = 'client_train'
+DUMMY_EVENT = 'dummy_event'
+UPLOAD_MODEL = 'upload_model'
